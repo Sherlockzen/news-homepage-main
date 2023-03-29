aca99c13c10d5f3b81056250f7b749d6db0932b2
Hewerton <hewertonlourenco@gmail.com>
2023-03-28 22:31:16 -0300

    Feat: add the side menu on mobile design

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const menu = document.querySelector(".menu")
 const btnOpen = document.getElementById('open_menu')
 const btnClose = document.getElementById('close_menu')
+const sideMenu = document.querySelector('.sidenav')
 
 menu.addEventListener('click', (e) => {
     btnOpen.classList.toggle("svg_hide")
     btnClose.classList.toggle("svg_hide")
+    if (btnClose.classList.contains("svg_hide")) {
+        sideMenu.style.width = "0";
+        document.body.style.overflowY = 'auto'
+    } else {
+        sideMenu.style.width = "20rem";
+        document.body.style.overflowY = 'hidden'
+    }
 })
